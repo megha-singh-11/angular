d6bb3b939060d799e2902f2ffd14c2cda6e030cb
megha-singh-11 <98537847+megha-singh-11@users.noreply.github.com>
2022-08-02 15:27:15 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,17 @@ export class AppComponent {
   sample= 'megha';
    sample= 'megha';
   sample= 'megha';
+  
+  add1(){
+    var c=2+5;
+  }
+   add2(){
+    var c=2+5;
+  }
+   add3(){
+    var c=2+5;
+  }
+   add4(){
+    var c=2+5;
+  }
 }
