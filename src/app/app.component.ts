8fd77dcfa14725ade6742a367415947407f2769d
megha-singh-11 <98537847+megha-singh-11@users.noreply.github.com>
2022-08-02 15:39:06 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang';
-  let sample= 'megha';
-  let sample= 'megha';
+   sample= 'megha';
+  sample= 'megha';
    sample= 'megha';
   sample= 'megha';
    sample= 'megha';
